a7809a28b2d2d0057a4d6002e22018cc9a351ccf
FallenShard <nemanjabartolovic@yahoo.com>
2017-03-24 21:44:10 +0100

    Change shader compilation script to ignore files older than 5 minutes

--- Crisp/Crisp/Source/Shaders/compile-vulkan.sh
+++ Crisp/Crisp/Source/Shaders/compile-vulkan.sh
@@ -1,16 +1,25 @@
 mkdir -p "../../Resources/Shaders"
 
-rm -r ../../Resources/Shaders/*
+#rm -r ../../Resources/Shaders/*
 
 errorPrefix="ERROR"
 
 for entry in *.glsl;
 do
+    dateModified=$(date +%s -r $entry) # in epoch seconds
+    cond=$(date +%s)
+    cond=$((cond-300)) # current time - 5 minutes
+    if [[ "$dateModified" < "$cond" ]] # Skip the file if it hasn't been modified in last 5 minutes
+    then
+        continue
+    fi
+
     shaderType=$(echo "$entry" | grep -o '\-[a-z]*\.')
     shaderType=${shaderType:1:4}
     techniqueName=$(echo "$entry" | grep -o '.*\.')
     techniqueName=${techniqueName:0:${#techniqueName}-6}
     output="../../Resources/Shaders/"$techniqueName"-"$shaderType".spv"
+    rm "$output"
 
     maxMessageLength=60
     message=$(printf "Compiling %q to SPIR-V" "$entry")
