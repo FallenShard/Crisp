792c6e17a95010550338b5c2956a8065b0187f11
FallenShard <nemanjabartolovic@yahoo.com>
2017-03-31 19:29:05 +0200

    Add common IORs to Fresnel

--- Crisp/Vesper/Vesper/Core/Fresnel.hpp
+++ Crisp/Vesper/Vesper/Core/Fresnel.hpp
@@ -4,6 +4,17 @@
 
 namespace vesper
 {
+    enum class IndexOfRefraction
+    {
+        Vacuum,
+        Air,
+        Ice,
+        Water,
+        Glass,
+        Sapphire,
+        Diamond
+    };
+
     class Fresnel
     {
     public:
@@ -35,5 +46,20 @@ namespace vesper
             float Rp = (etaT * cosThetaI - etaI * cosThetaT) / (etaT * cosThetaI + etaI * cosThetaT);
             return (Rs * Rs + Rp * Rp) / 2.0f;
         }
+
+        float getIOR(IndexOfRefraction iorMaterial)
+        {
+            switch (iorMaterial)
+            {
+            case IndexOfRefraction::Vacuum:   return 1.0f;
+            case IndexOfRefraction::Air:      return 1.00029f;
+            case IndexOfRefraction::Ice:      return 1.31f;
+            case IndexOfRefraction::Water:    return 1.33f;
+            case IndexOfRefraction::Glass:    return 1.52f;
+            case IndexOfRefraction::Sapphire: return 1.77f;
+            case IndexOfRefraction::Diamond:  return 2.417f;
+            default:                          return 1.0f;
+            }
+        };
     };
 }
