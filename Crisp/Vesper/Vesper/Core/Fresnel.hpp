4584ea3e5b78533a805059a0a679a46a3a7cd54c
FallenShard <nemanjabartolovic@yahoo.com>
2017-04-06 12:42:13 +0200

    Add an overload for Fresnel dielectric to get cosThetaT

--- Crisp/Vesper/Vesper/Core/Fresnel.hpp
+++ Crisp/Vesper/Vesper/Core/Fresnel.hpp
@@ -18,6 +18,35 @@ namespace vesper
     class Fresnel
     {
     public:
+        inline static float dielectric(float cosThetaI, float extIOR, float intIOR, float& cosThetaT)
+        {
+            float etaI = extIOR, etaT = intIOR;
+
+            // If indices of refraction are the same, no fresnel effects
+            if (extIOR == intIOR)
+                return 0.0f;
+
+            // if cosThetaI is < 0, it means the ray is coming from inside the object
+            if (cosThetaI < 0.0f)
+            {
+                std::swap(etaI, etaT);
+                cosThetaI = -cosThetaI;
+            }
+
+            float eta = etaI / etaT;
+            float sinThetaTSqr = eta * eta * (1.0f - cosThetaI * cosThetaI);
+
+            // Total internal reflection
+            if (sinThetaTSqr > 1.0f)
+                return 1.0f;
+
+            cosThetaT = std::sqrtf(1.0f - sinThetaTSqr);
+
+            float Rs = (etaI * cosThetaI - etaT * cosThetaT) / (etaI * cosThetaI + etaT * cosThetaT);
+            float Rp = (etaT * cosThetaI - etaI * cosThetaT) / (etaT * cosThetaI + etaI * cosThetaT);
+            return (Rs * Rs + Rp * Rp) / 2.0f;
+        }
+
         inline static float dielectric(float cosThetaI, float extIOR, float intIOR)
         {
             float etaI = extIOR, etaT = intIOR;
