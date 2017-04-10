b8a1e16f37ed5af9adb2a91f93be322362c41686
FallenShard <nemanjabartolovic@yahoo.com>
2017-04-10 20:20:03 +0200

    Externalize microfacet distributions

---
+++ Crisp/Vesper/Vesper/BSDFs/MicrofacetDistributions/MicrofacetDistribution.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "Spectrums/Spectrum.hpp"
+#include "Core/VariantMap.hpp"
+
+namespace vesper
+{
+    class MicrofacetDistribution
+    {
+    public:
+        virtual ~MicrofacetDistribution() {};
+
+        virtual glm::vec3 sampleNormal(const glm::vec2& sample) const = 0;
+        virtual float pdf(const glm::vec3& m) const = 0;
+
+        virtual float D(const glm::vec3& m) const = 0;
+        virtual float G(const glm::vec3& wi, const glm::vec3& wo, const glm::vec3& m) const = 0;
+
+    protected:
+        float m_alpha;
+    };
+}
