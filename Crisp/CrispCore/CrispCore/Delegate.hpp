01578812c28121ed40f62ab82f493b35e3adfeed
FallenShard <nemanjabartolovic@yahoo.com>
2017-09-10 10:26:35 +0200

    Add a project to encapsulate common types in a library

---
+++ Crisp/CrispCore/CrispCore/Delegate.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+namespace crisp
+{
+    template<typename ReturnType, typename... ParamTypes>
+    class Delegate
+    {
+        using CallbackType = ReturnType(*)(void* receiverObject, ParamTypes...);
+
+    public:
+        Delegate(void* receiverObject, CallbackType function)
+            : m_receiverObject(receiverObject)
+            , m_callbackFunction(function)
+        {
+        }
+
+        // Creates a delegate from specified method
+        template<typename T, ReturnType(T::*TMethod)(ParamTypes...)>
+        static Delegate fromFunction(T* receiverObject)
+        {
+            return Delegate(receiverObject, &methodCaller<T, TMethod>);
+        }
+
+        template<ReturnType(*SMethod)(ParamTypes...)>
+        static Delegate fromStaticFunction()
+        {
+            return Delegate(nullptr, &staticMethodCaller<SMethod>);
+        }
+
+        // Executes the stored callback
+        ReturnType operator()(ParamTypes... args) const
+        {
+            return (*m_callbackFunction)(m_receiverObject, args...);
+        }
+
+        // For storage in containers as a key
+        bool operator<(const Delegate& other) const
+        {
+            if (m_receiverObject < other.m_receiverObject)
+                return true;
+            else if (m_receiverObject == other.m_receiverObject)
+                return m_callbackFunction < other.m_callbackFunction;
+            else
+                return false;
+        }
+
+    private:
+        void*        m_receiverObject;
+        CallbackType m_callbackFunction;
+
+        // Where the magic happens
+        template<typename T, ReturnType(T::*TMethod)(ParamTypes...)>
+        static ReturnType methodCaller(void* receiverObject, ParamTypes... args)
+        {
+            return (static_cast<T*>(receiverObject)->*TMethod)(args...);
+        }
+
+        template<ReturnType(*SMethod)(ParamTypes...)>
+        static ReturnType staticMethodCaller(void* receiverObject, ParamTypes... args)
+        {
+            return (*SMethod)(args...);
+        }
+    };
+}
---
+++ Crisp/CrispCore/CrispCore/Event.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <set>
+#include <vector>
+#include <functional>
+
+#include "Delegate.hpp"
+
+namespace crisp
+{
+    template<typename ...ParamTypes>
+    class Event
+    {
+    public:
+        Event() = default;
+        ~Event() = default;
+
+        Event(const Event& other) = delete;
+        Event<void, ParamTypes...>& operator=(const Event& other) = delete;
+        Event<void, ParamTypes...>& operator=(Event&& other) = delete;
+
+        template<typename T, void(T::*callbackMethod)(ParamTypes...)>
+        void subscribe(T* obj)
+        {
+            m_delegates.insert(Delegate<void, ParamTypes...>::fromFunction<T, callbackMethod>(obj));
+        }
+
+        template<void(*callbackMethod)(ParamTypes...)>
+        void subscribe()
+        {
+            m_delegates.insert(Delegate<void, ParamTypes...>::fromStaticFunction<callbackMethod>());
+        }
+
+        template <typename FuncType>
+        void subscribe(FuncType&& func)
+        {
+            m_functors.emplace_back(std::forward<FuncType>(func));
+        }
+
+        template<typename T>
+        void subscribe(Delegate<void, ParamTypes...> del)
+        {
+            m_delegates.insert(del);
+        }
+
+        template<typename T, void(T::*callbackMethod)(ParamTypes...)>
+        void unsubscribe(T* obj)
+        {
+            m_delegates.erase(Delegate<void, ParamTypes...>::fromFunction<T, callbackMethod>(obj));
+        }
+
+        void operator()(ParamTypes... args)
+        {
+            for (auto& delegate : m_delegates)
+                delegate(args...);
+
+            for (auto& func : m_functors)
+                func(args...);
+        }
+
+    private:
+        std::set<Delegate<void, ParamTypes...>>         m_delegates;
+        std::vector<std::function<void(ParamTypes...)>> m_functors;
+    };
+}
