6ca6d0a673459d4cbb68509223e018d3593f8b8e
FallenShard <nemanjabartolovic@gmail.com>
2023-11-19 19:39:53 +0100

    Add utilities to show camera info in ImGui

---
+++ Crisp/Crisp/GUI/ImGuiCameraUtils.cpp
@@ -0,0 +1,18 @@
+#include <Crisp/Gui/ImGuiCameraUtils.hpp>
+
+#include <imgui.h>
+
+#include <Crisp/Core/Format.hpp>
+#include <Crisp/Utils/GlmFormatters.hpp>
+
+namespace crisp {
+void drawCameraUi(const Camera& camera) {
+    ImGui::Begin("Camera");
+    ImGui::LabelText("Position", fmt::format("{}", camera.getPosition()).c_str());
+    ImGui::LabelText("Look", fmt::format("{}", camera.getLookDir()).c_str());
+    ImGui::LabelText("Up", fmt::format("{}", camera.getUpDir()).c_str());
+    ImGui::LabelText("Right", fmt::format("{}", camera.getRightDir()).c_str());
+    ImGui::LabelText("Depth Range", fmt::format("{}", camera.getViewDepthRange()).c_str());
+    ImGui::End();
+}
+} // namespace crisp
---
+++ Crisp/Crisp/GUI/ImGuiCameraUtils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Crisp/Camera/Camera.hpp>
+
+namespace crisp {
+
+void drawCameraUi(const Camera& camera);
+
+} // namespace crisp
