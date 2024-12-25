44d69d3a7a2a838d3186c3d2df3e6e31a8c1ac3f
FallenShard <nemanjabartolovic@gmail.com>
2024-12-25 16:20:32 +0100

    Add utilities for macro-based logging

--- Crisp/Crisp/Core/Logger.hpp
+++ Crisp/Crisp/Core/Logger.hpp
@@ -2,6 +2,7 @@
 
 #include <Crisp/Core/Format.hpp>
 
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
@@ -16,4 +17,18 @@ inline std::shared_ptr<spdlog::logger> createLoggerSt(std::string_view loggerNam
 inline std::shared_ptr<spdlog::logger> createLoggerMt(std::string_view loggerName) {
     return spdlog::stdout_color_mt(std::string(loggerName));
 }
+
+inline void setDefaultSpdlogPattern() {
+    spdlog::set_pattern("%^[%T.%e][%t][%n][%l]%$\033[34m[%#]\033[0m: %v");
+}
 } // namespace crisp
+
+#define CRISP_MAKE_LOGGER_ST(channelName) auto logger = ::crisp::createLoggerSt(channelName);
+#define CRISP_MAKE_LOGGER_MT(channelName) auto logger = ::crisp::createLoggerMt(channelName);
+
+#define CRISP_LOGT(...) SPDLOG_LOGGER_TRACE(logger, __VA_ARGS__);
+#define CRISP_LOGD(...) SPDLOG_LOGGER_DEBUG(logger, __VA_ARGS__);
+#define CRISP_LOGI(...) SPDLOG_LOGGER_INFO(logger, __VA_ARGS__);
+#define CRISP_LOGW(...) SPDLOG_LOGGER_WARN(logger, __VA_ARGS__);
+#define CRISP_LOGE(...) SPDLOG_LOGGER_ERROR(logger, __VA_ARGS__);
+#define CRISP_LOGF(...) SPDLOG_LOGGER_CRITICAL(logger, __VA_ARGS__);
