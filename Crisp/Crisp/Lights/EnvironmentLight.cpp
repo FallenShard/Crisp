78a4b4cc2731419d38babac5c51e147523010ae5
FallenShard <nemanjabartolovic@gmail.com>
2026-08-02 02:16:26 +0200

    Migrate EnvironmentLight passes to dynamic rendering

--- Crisp/Crisp/Lights/EnvironmentLight.cpp
+++ Crisp/Crisp/Lights/EnvironmentLight.cpp
@@ -3,9 +3,8 @@
 #include <Crisp/Geometry/Geometry.hpp>
 #include <Crisp/Mesh/TriangleMeshUtils.hpp>
 #include <Crisp/Renderer/Material.hpp>
-#include <Crisp/Renderer/RenderPasses/CubeMapRenderPass.hpp>
-#include <Crisp/Renderer/RenderPasses/TexturePass.hpp>
 #include <Crisp/Renderer/VulkanImageUtils.hpp>
+#include <Crisp/Vulkan/Rhi/VulkanRasterizationPassDescriptor.hpp>
 
 namespace crisp {
 namespace {
@@ -18,6 +17,29 @@ const std::array<glm::mat4, kCubeMapFaceCount> kCubeMapViews = {
     glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, -1.0f, 0.0f)),
     glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f)),
 };
+
+void beginColorRendering(
+    const VulkanCommandEncoder& commandEncoder, const VkImageView imageView, const VkExtent2D renderArea) {
+    const VkRenderingAttachmentInfo colorAttachment{
+        .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO,
+        .imageView = imageView,
+        .imageLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+        .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+        .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+        .clearValue = VkClearValue{.color{{0.0f, 0.0f, 0.0f, 1.0f}}},
+    };
+    const VkRenderingInfo renderingInfo{
+        .sType = VK_STRUCTURE_TYPE_RENDERING_INFO,
+        .renderArea = {.offset = {0, 0}, .extent = renderArea},
+        .layerCount = 1,
+        .colorAttachmentCount = 1,
+        .pColorAttachments = &colorAttachment,
+    };
+    commandEncoder.beginRendering(renderingInfo);
+    commandEncoder.setViewport(
+        {0.0f, 0.0f, static_cast<float>(renderArea.width), static_cast<float>(renderArea.height), 0.0f, 1.0f});
+    commandEncoder.setScissor({.offset = {0, 0}, .extent = renderArea});
+}
 } // namespace
 
 EnvironmentLight::EnvironmentLight(Renderer& renderer, const ImageBasedLightingData& iblData) {
@@ -58,82 +80,59 @@ std::unique_ptr<VulkanImage> convertEquirectToCubeMap(Renderer* renderer, const 
             .usageFlags = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
                           VK_IMAGE_USAGE_TRANSFER_SRC_BIT | additionalFlags,
             .createFlags = VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT,
-
         });
     device.setObjectName(*cubeMap, "CubeMap");
 
-    auto cubeMapPass = createTexturePass(device, {cubeMapSize, cubeMapSize}, cubeMap->getFormat());
-
-    std::vector<std::unique_ptr<VulkanPipeline>> cubeMapPipelines(1);
-    std::vector<std::unique_ptr<VulkanFramebuffer>> cubeMapFramebuffers(kCubeMapFaceCount);
+    const VkExtent2D cubeMapExtent{cubeMapSize, cubeMapSize};
+    const VulkanRasterizationPassDescriptor rasterizationPassDescriptor{
+        .colorAttachmentFormats = {cubeMap->getFormat()},
+    };
+    auto cubeMapPipeline = renderer->createPipeline("EquirectToCube.json", rasterizationPassDescriptor);
     std::vector<std::unique_ptr<VulkanImageView>> cubeMapImageViews(kCubeMapFaceCount);
     for (uint32_t i = 0; i < kCubeMapFaceCount; ++i) {
-        if (i == 0) {
-            cubeMapPipelines[i] = renderer->createPipeline("EquirectToCube.json", *cubeMapPass, i);
-        }
         cubeMapImageViews[i] = createView(device, *cubeMap, VK_IMAGE_VIEW_TYPE_2D, i, 1, 0, 1);
-        cubeMapFramebuffers[i] = std::make_unique<VulkanFramebuffer>(
-            device,
-            cubeMapPass->getHandle(),
-            VkExtent2D{cubeMapSize, cubeMapSize},
-            std::vector<VkImageView>{cubeMapImageViews[i]->getHandle()});
     }
 
     const VertexLayoutDescription vertexLayout = {{VertexAttribute::Position}};
     const auto unitCube = createGeometry(*renderer, createCubeMesh(), vertexLayout);
     auto sampler = std::make_unique<VulkanSampler>(
         renderer->getDevice(), VK_FILTER_LINEAR, VK_FILTER_LINEAR, VK_SAMPLER_ADDRESS_MODE_REPEAT, 16.0f, 12.0f);
-    auto cubeMapMaterial = std::make_unique<Material>(cubeMapPipelines[0].get());
+    auto cubeMapMaterial = std::make_unique<Material>(cubeMapPipeline.get());
     cubeMapMaterial->writeDescriptor(0, 0, equirectMap.getView().getDescriptorInfo(sampler.get()));
     renderer->getDevice().flushDescriptorUpdates();
 
     device.getGeneralQueue().submitAndWait(
-        [&unitCube, &cubeMapPipelines, &cubeMapPass, &cubeMapMaterial, &cubeMapFramebuffers, &cubeMap](
+        [&unitCube, &cubeMapPipeline, &cubeMapMaterial, &cubeMapImageViews, &cubeMap, cubeMapExtent](
             const VkCommandBuffer cmdBuffer) {
             VulkanCommandEncoder commandEncoder(cmdBuffer);
 
             commandEncoder.insertBarrier(kTransferWrite >> (kIndexInputRead | kVertexInputRead));
-            commandEncoder.transitionLayout(
-                *cubeMap,
-                VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-                kNullStage >> kFragmentRead,
-                cubeMap->getFirstMipRange());
+            for (uint32_t i = 0; i < kCubeMapFaceCount; ++i) {
+                const VkImageSubresourceRange faceRange{
+                    .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                    .baseMipLevel = 0,
+                    .levelCount = 1,
+                    .baseArrayLayer = i,
+                    .layerCount = 1,
+                };
+                commandEncoder.transitionLayout(
+                    *cubeMap, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, kNullStage >> kColorWrite, faceRange);
+                beginColorRendering(commandEncoder, cubeMapImageViews[i]->getHandle(), cubeMapExtent);
 
-            const glm::mat4 captureProjection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 10.0f);
-            const std::array<glm::mat4, kCubeMapFaceCount> captureViews = {
-                glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f)),
-                glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f)),
-                glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
-                glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f)),
-                glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, -1.0f, 0.0f)),
-                glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f))};
-
-            for (uint32_t i = 0; i < kCubeMapFaceCount; i++) {
-                commandEncoder.beginRenderPass(*cubeMapPass, *cubeMapFramebuffers[i]);
-
-                glm::mat4 MVP = captureProjection * captureViews[i];
+                const glm::mat4 MVP = kCubeFaceProjection * kCubeMapViews[i];
                 std::vector<char> pushConst(sizeof(glm::mat4));
                 memcpy(pushConst.data(), glm::value_ptr(MVP), sizeof(glm::mat4));
 
-                commandEncoder.bindPipeline(*cubeMapPipelines[0]);
-                cubeMapPipelines[0]->getPipelineLayout()->setPushConstants(cmdBuffer, pushConst.data());
+                commandEncoder.bindPipeline(*cubeMapPipeline);
+                cubeMapPipeline->getPipelineLayout()->setPushConstants(cmdBuffer, pushConst.data());
 
                 cubeMapMaterial->bind(cmdBuffer);
                 unitCube.bindAndDraw(cmdBuffer);
-                commandEncoder.endRenderPass(*cubeMapPass);
-                cubeMap->setImageLayout(
-                    VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
-                    {
-                        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-                        .baseMipLevel = 0,
-                        .levelCount = 1,
-                        .baseArrayLayer = i,
-                        .layerCount = 1,
-                    });
+                commandEncoder.endRendering();
             }
             cubeMap->transitionLayout(
                 cmdBuffer, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, 0, kCubeMapFaceCount, 0, 1, kColorWrite >> kTransferRead);
-            cubeMap->buildMipmaps(cmdBuffer);
+            cubeMap->buildMipmaps(cmdBuffer, kNullStage);
             cubeMap->transitionLayout(
                 cmdBuffer, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, kTransferWrite >> kFragmentSampledRead);
         });
@@ -142,36 +141,32 @@ std::unique_ptr<VulkanImage> convertEquirectToCubeMap(Renderer* renderer, const 
 
 std::unique_ptr<VulkanImage> integrateBrdfLut(Renderer* renderer) {
     auto& device = renderer->getDevice();
+    constexpr VkExtent2D kBrdfLutExtent{512, 512};
 
     auto brdfLut = std::make_unique<VulkanImage>(
         device,
         VulkanImageDescription{
             .format = VK_FORMAT_R16G16_SFLOAT,
-            .extent = {512, 512, 1},
+            .extent = {kBrdfLutExtent.width, kBrdfLutExtent.height, 1},
             .usageFlags = VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
         });
     auto view = createView(device, *brdfLut, VK_IMAGE_VIEW_TYPE_2D);
-    auto texPass = createTexturePass(renderer->getDevice(), VkExtent2D{512, 512}, VK_FORMAT_R16G16_SFLOAT);
-
-    auto framebuffer = std::make_unique<VulkanFramebuffer>(
-        device, texPass->getHandle(), view->getImage().getExtent2D(), std::vector<VkImageView>{view->getHandle()});
-
-    std::vector<std::unique_ptr<VulkanFramebuffer>> cubeMapFramebuffers(kCubeMapFaceCount);
-
-    auto pipeline = renderer->createPipeline("BrdfLut.json", *texPass, 0);
+    const VulkanRasterizationPassDescriptor rasterizationPassDescriptor{
+        .colorAttachmentFormats = {brdfLut->getFormat()},
+    };
+    auto pipeline = renderer->createPipeline("BrdfLut.json", rasterizationPassDescriptor);
 
     renderer->getDevice().getGeneralQueue().submitAndWait(
-        [renderer, &pipeline, &texPass, &framebuffer, &brdfLut](VkCommandBuffer cmdBuffer) {
+        [renderer, &pipeline, &view, &brdfLut](VkCommandBuffer cmdBuffer) {
             VulkanCommandEncoder commandEncoder(cmdBuffer);
             commandEncoder.transitionLayout(
-                *brdfLut, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, kNullStage >> kFragmentRead);
-            commandEncoder.beginRenderPass(*texPass, *framebuffer);
+                *brdfLut, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, kNullStage >> kColorWrite);
+            beginColorRendering(commandEncoder, view->getHandle(), brdfLut->getExtent2D());
 
             commandEncoder.bindPipeline(*pipeline);
             renderer->drawFullScreenQuad(cmdBuffer);
 
-            commandEncoder.endRenderPass(*texPass);
-            brdfLut->setImageLayout(VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, brdfLut->getFullRange());
+            commandEncoder.endRendering();
             commandEncoder.transitionLayout(
                 *brdfLut, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, kColorWrite >> kFragmentSampledRead);
         });
